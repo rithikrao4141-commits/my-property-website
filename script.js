d6c092dbfaa39758685665e6866fdb679034d45a
rithikrao4141-commits <rithikrao4141@gmail.com>
2026-04-13 18:12:01 +0530

    Update title from 'Luxury Property' to 'Luxury Property Dealer'

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 let slides = document.querySelectorAll('.slide');
 let
+
+
+
+let slides = document.querySelectorAll('.slide');
+let index = 0;
+
+// Show slide function
+function showSlide(i) {
+  slides.forEach((slide, idx) => {
+    slide.style.display = "none";
+  });
+  slides[i].style.display = "block";
+}
+
+// Next slide
+function nextSlide() {
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
+  showSlide(index);
+}
+
+// Prev slide
+function prevSlide() {
+  index--;
+  if (index < 0) {
+    index = slides.length - 1;
+  }
+  showSlide(index);
+}
+
+// Auto slide every 4 sec
+setInterval(nextSlide, 4000);
+
+// Button controls
+document.querySelector('.next').addEventListener('click', nextSlide);
+document.querySelector('.prev').addEventListener('click', prevSlide);
+
+// Init
+showSlide(index);
