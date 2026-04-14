2e26aee02dd6019d91809145fc3a10cb76a95f4e
rithikrao4141-commits <rithikrao4141@gmail.com>
2026-04-14 22:48:04 +0530

    Update title from 'Luxury Property' to 'Luxury Property Dealer'

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let dots = document.querySelectorAll(".dot");
 
 let current = 0;
 
-// SHOW
 function showSlide(index) {
   slides.forEach((slide, i) => {
     slide.classList.remove("active");
@@ -14,16 +13,13 @@ function showSlide(index) {
   dots[index].classList.add("active");
 }
 
-// NEXT
 function nextSlide() {
   current = (current + 1) % slides.length;
   showSlide(current);
 }
 
-// AUTO
 setInterval(nextSlide, 3000);
 
-// BUTTONS
 document.querySelector(".next").onclick = nextSlide;
 
 document.querySelector(".prev").onclick = () => {
@@ -31,28 +27,9 @@ document.querySelector(".prev").onclick = () => {
   showSlide(current);
 };
 
-// DOT CLICK
 dots.forEach((dot, i) => {
   dot.onclick = () => {
     current = i;
     showSlide(current);
   };
-});
-
-// SWIPE (MOBILE)
-let startX = 0;
-
-document.querySelector(".slider").addEventListener("touchstart", (e) => {
-  startX = e.touches[0].clientX;
-});
-
-document.querySelector(".slider").addEventListener("touchend", (e) => {
-  let endX = e.changedTouches[0].clientX;
-
-  if (startX > endX + 50) {
-    nextSlide();
-  } else if (startX < endX - 50) {
-    current = (current - 1 + slides.length) % slides.length;
-    showSlide(current);
-  }
 });
