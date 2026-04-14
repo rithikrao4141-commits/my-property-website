3967d31c342062b9c65e15bcbe3e753098f8f6d8
rithikrao4141-commits <rithikrao4141@gmail.com>
2026-04-14 22:42:19 +0530

    Update title from 'Luxury Property' to 'Luxury Property Dealer'

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// SELECT
 let slides = document.querySelectorAll(".slide");
+let dots = document.querySelectorAll(".dot");
+
 let current = 0;
 
-// FUNCTION
+// SHOW
 function showSlide(index) {
   slides.forEach((slide, i) => {
     slide.classList.remove("active");
-    if (i === index) {
-      slide.classList.add("active");
-    }
+    dots[i].classList.remove("active");
   });
+
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
 }
 
 // NEXT
@@ -18,12 +20,39 @@ function nextSlide() {
   showSlide(current);
 }
 
-// AUTO SLIDE (3 sec)
+// AUTO
 setInterval(nextSlide, 3000);
 
 // BUTTONS
 document.querySelector(".next").onclick = nextSlide;
+
 document.querySelector(".prev").onclick = () => {
   current = (current - 1 + slides.length) % slides.length;
   showSlide(current);
 };
+
+// DOT CLICK
+dots.forEach((dot, i) => {
+  dot.onclick = () => {
+    current = i;
+    showSlide(current);
+  };
+});
+
+// SWIPE (MOBILE)
+let startX = 0;
+
+document.querySelector(".slider").addEventListener("touchstart", (e) => {
+  startX = e.touches[0].clientX;
+});
+
+document.querySelector(".slider").addEventListener("touchend", (e) => {
+  let endX = e.changedTouches[0].clientX;
+
+  if (startX > endX + 50) {
+    nextSlide();
+  } else if (startX < endX - 50) {
+    current = (current - 1 + slides.length) % slides.length;
+    showSlide(current);
+  }
+});
